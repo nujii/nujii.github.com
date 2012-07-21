031d6afcd55b61f8d601deb46121218074932aae
Cameron Bytheway <cameron@nujii.com>
2012-07-20 21:46:15 -0600

    got the macbook working for the most part

---
+++ assets/themes/nujii/javascripts/home.js
@@ -0,0 +1,28 @@
+
+(function() {
+  var container = $('.macbook-container');
+  var navBarHeight = $('.navbar').height();
+  var footerHeight = $('footer').height();
+
+  var moveMacbook = function() {
+    var imageHeight = $('.macbook img').height();
+    var introHeight = $('.intro').height();
+    var bodyHeight = $('body').height();
+    var windowWidth = $(window).width();
+    if (windowWidth > 786) {
+      var contentHeight = bodyHeight-footerHeight-navBarHeight-60;
+      var spanHeight = (imageHeight>introHeight)?imageHeight:introHeight;
+      var newHeight = (spanHeight>contentHeight)?spanHeight:contentHeight;
+      container.height(newHeight);
+    }
+    else {
+      var contentHeight = footerHeight+navBarHeight+introHeight+65;
+      var newHeight = (contentHeight+imageHeight<bodyHeight)?(bodyHeight-contentHeight):(contentHeight-imageHeight);
+      container.height(newHeight);
+    }
+  };
+
+  $("body").resize(moveMacbook);
+  moveMacbook();
+  
+}).call(this);
